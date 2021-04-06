15661a275a853a88cbafacb8ec3a13d2cfffc771
younsoolim <ylim27@my.bcit.ca>
2021-04-06 14:01:21 -0700

    Fixed storybook

--- revamp/stories/Location.stories.js
+++ revamp/stories/Location.stories.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import Location from '../comps/Location';
 
+export default {
+  title: "Location",
+  component: <Location></Location>
+}
+
 export const DefaultLocation  = () => <Location /> 
-export const FirstLocation  = () => <Location title="Location 2:" text = "Wildlife Thrift Store Downtown Vancouver" /> 
+export const SecondLocation  = () => <Location title="Location 2:" text = "Wildlife Thrift Store Downtown Vancouver" /> 
+export const ThirdLocation  = () => <Location title="Location 3:" text = "The Salvation Army -Belkin House Downtown Vancouver" /> 
+
