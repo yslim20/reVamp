ebda1e82ec8c8f3212ab33ce640b56902085e410
mmonay24 <marabelle.monay@gmail.com>
2021-04-15 13:32:49 -0700

    comps file

---
+++ revamp/comps/FinishedLook/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+const FinishedLookCont = styled.div `
+    display: flex;
+    flex-direction: column;
+`;
+const FinishedLookTitle = styled.h2`
+    color: black;
+    font-size: 22px;
+    margin-bottom: 5px;
+    font-weight: 400;
+`;
+
+
+const FinishedLookPic = styled.img`
+    border-radius:10px;
+    width: 295px;
+    border-style: solid;
+    border-color: #CCCCCC;
+    border-width: 1px;
+`;
+
+
+//props
+const FinishedLook = ({
+    title="Finished Look!",
+    picture="/sweater-beanie.svg",
+}) =>{
+
+    return <FinishedLookCont>
+        <FinishedLookTitle>{title}</FinishedLookTitle>
+        
+        <FinishedLookPic src={picture}></FinishedLookPic>
+      
+    </FinishedLookCont>
+
+}
+
+export default FinishedLook;
