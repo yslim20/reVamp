47f7a50fdc79fca8b625a242f20604857a499142
irenewuu <irenewu222@gmail.com>
2021-04-16 11:29:14 -0700

    initial push

--- revamp/pages/index.js
+++ revamp/pages/index.js
@@ -1,59 +1,40 @@
 import Head from 'next/head'
-import FinishedLook from '../comps/FinishedLook';
-import Background from '../comps/Background';
-import DIYs from '../comps/DIYs';
+import styled from 'styled-components';
+import React, {useState} from 'react';
+import {useRouter} from 'next/router';
+import DRButton from '../comps/DRButtons';
+
+const HomeCont = styled.div``;  
+
 
 export default function Home() {
-  return <div>
-    <FinishedLook picture="/Denim-Flower.svg"/>
-    <FinishedLook></FinishedLook>
-    <Background></Background>
-    <DIYs></DIYs>
-    <DIYs title="demin flowers" picture="/Denim-Flower.svg"/>
 
-  </div>
-// import Header from '../comps/Header/index';
-// import BottomNav from '../comps/BottomNav/index';
-// import TopNav from '../comps/TopNav/index';
+  const router = useRouter();
+  const {places} = router.query;
 
-// export default function Home() {
-//   return <div>
-//     <TopNav />
-//     <Header text="what would you like to repurpose?"/>
-//     <BottomNav />
-//   </div>
-// }
+  return (<HomeCont>
+    <Head>
+      <title className = "title">reVamp</title>
+    </Head>
+    
+    <div className="container home_cont flex_col"> 
 
-// import styles from '../styles/Home.module.css'
+      <img className = "backG topL" src = "/Index_01_TopL.svg" alt = "background image" />
+      <img className = "backG topR" src = "/Index_02_TopR.svg" alt = "background image" />
+      <img className = "backG bttmL" src = "/Index_03_BttmL.svg" alt = "background image" />
+      <img className = "backG bttmR" src = "/Index_04_BttmR.svg" alt = "background image" />     
 
-// import styled from 'styled-components';
-// import React, {useState} from 'react';
+      <div className="logo_cont flex_col">          
+          <img className = "logo" src = "/homeLogo.svg" alt = "logo" />
+      </div>
 
-// import SingleLocation from '../comps/SingleLocation';
+      <DRButton 
+        onClick = {() => router.push("welcome")}
+        shadow = "0px 2px 4px rgba(0,0,0,.25)" border = "none" text = "start" size = "1.25em">
+      </DRButton>
 
-// const HomeCont = styled.div`
-  
+    </div>
 
-
-// export default function Home() {
-//   return (<HomeCont>
-//     <Head>
-//       <title className = "title">reVamp</title>
-//     </Head>
-
-//     <div className="container home_cont flex_col">
-
-//       <div className="logo_cont flex_col">          
-//           <img className = "logo" src = "/homeLogo.svg" alt = "logo" />
-//       </div>
-
-//       <div className = "buttonCont flex_col">
-//         button
-//       </div>
-//     </div>
-
-//   </HomeCont>
-//   )
-// }
-
+  </HomeCont>
+  )
 };
