8ac7918fecb569a3bc20b51f100f29747f4ec1da
younsoolim <ylim27@my.bcit.ca>
2021-04-26 15:33:47 -0700

    welcome page updated with animation

--- revamp/pages/welcome.js
+++ revamp/pages/welcome.js
@@ -44,18 +44,22 @@ export default function Home() {
         <Background>  
           <div className = "flex_col paddingB-40">
             <Header text = "welcome!" h2Size = "2.5em"></Header>
+            
             <div className = "welcome_sub">
-              click the button below to learn about the impact of fast fashion on Sustainability.
+              <img className = "welcome_icon" src = "/icon_recycle_hand.svg" alt = "recycle icon" />
+              <img className = "welcome_icon globe_icon" src = "/icon_globe.svg" alt = "globe icon" />
+              <p>on this app, you will be able to make sustainability your top priority by being able to learn how to repurpose your clothes, donate your old garments, or by simply learning about the impact of fast fashion on our planet.</p>
             </div>
 
             <DRButton 
               onClick = {() => router.push("infographic")}
-              shadow = "0px 2px 4px rgba(0,0,0,.25)" text = "sustainability Infographic" size = "1em">
+              shadow = "0px 2px 4px rgba(0,0,0,.25)" text = "Get Started" size = "1em">
             </DRButton>
 
             <div className = "skip">
+              <img className = "recycle_icon" src ="/icon_recycle.svg" alt = "recycle icon" />
               <Link href ="/recycle">
-                <a className = "skipTxt">click here to skip to donate or repurpose</a>
+                <a className = "skipTxt">click here to learn more about sustainability</a>
               </Link>            
             </div>
           </div>
