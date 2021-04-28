8304c6f1270671bb1a31d5b53a9aa89af3bb7efb
montanalw <72103298+montanalw@users.noreply.github.com>
2021-04-28 15:02:33 -0700

    infographic pt 2 and 3 done and box shadows

--- revamp/comps/Background/index.js
+++ revamp/comps/Background/index.js
@@ -19,15 +19,18 @@ const BackgroundDiv = styled.div`
     justify-content: center;
     align-items: center; 
     margin-bottom: 210px;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    background-color: ${props=>props.bgcol};
 `;
 
 const Background = ({
     children = null,
-    padding="40px 20px"
+    padding="40px 20px",
+    bgcol="#000000"
 }
 ) =>{
 
-    return <BackgroundDiv padding={padding}>
+    return <BackgroundDiv padding={padding} bgcol={bgcol}>
         {children}    
     </BackgroundDiv>
 }
--- revamp/pages/infographic.js
+++ revamp/pages/infographic.js
@@ -4,6 +4,7 @@ import Background from '../comps/Background';
 import TopNav from '../comps/TopNav';
 import BottomNav from '../comps/BottomNav';
 import Header from '../comps/Header';
+import Reduce from '../comps/Reduce';
 
 import styled from 'styled-components';
 import React, {useState} from 'react';
@@ -33,16 +34,68 @@ font-family: 'Montserrat', sans-serif;
 text-align: center;
 color: #333333;
 max-width: 250px;
+margin: 0;
 `
 
-const BgImg = styled.img`
-width: 341px;
-position: relative;
+const SubTitle = styled.p`
+font-family: 'Montserrat', sans-serif;
+text-align: center;
+color: #333333;
+max-width: 250px;
+font-size: 22px;
+font-weight: 550;
+margin-bottom: 50px;
 `
 
-const ImgCont = styled.div`
-position: relative;
-top: 45px;
+const PerCont = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin-top: 45px;
+`
+
+const Percent = styled.p`
+font-family: 'Montserrat', sans-serif;
+text-align: center;
+color: #50514F;
+max-width: 250px;
+font-size: 48px;
+font-weight: 700;
+margin: 0;
+`
+
+const RnR = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 35px;
+    margin-bottom: 35px;
+`
+
+const ReRe = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
+const BoldT = styled.p`
+font-family: 'Montserrat', sans-serif;
+text-align: center;
+font-weight: 700;
+`
+
+const Img = styled.img`
+min-height: 110px;
+min-width: 100px;
+`
+
+const Or = styled.p`
+margin: 85px 35px 0 35px;
+`
+
+const BigRec = styled.img`
+margin-top: 25px;
+margin-bottom: 25px;
 `
 
 //const Pile = {backgroundImage: 'url(/pileofclothes.svg)'}
@@ -56,15 +109,39 @@ export default function Home() {
 
     <div className="container flex_col">
         <TopNav />
-        <Background>      
+        <Background bgcol="#FFFFFF">      
           <div className = "flex_col">
-            <Header text = "sustainability and fast fashion" h2Size = "1.75em"></Header>          
+            <Header text = "clothing waste" h2Size = "1.65em"></Header>
+            <SubTitle>in North America,</SubTitle>
+            <img src="thrownaway.svg" />   
+            <PerCont>
+            <Info>and</Info>
+            <Percent>95%</Percent>
+            <Info>could have been</Info>
+            </PerCont>   
+            <RnR>
+            <ReRe>
+            <BoldT>repurposed</BoldT>
+            <Img src="repurp.svg"/>
+            </ReRe>
+            <Or>or</Or>
+            <ReRe>
+            <BoldT>recycled</BoldT>
+            <Img src="rec.svg"/>
+            </ReRe>
+            </RnR>
+            <Header text = "how to reduce environmental impact" h2Size = "1.65em"></Header>
+            <BigRec src="/bigrec.svg" />
+            <img src="/bigglobe.svg" className="globe_info"/>
+            <Reduce></Reduce>
+            <Reduce text="2. buy clothes with better quality so they last longer" 
+            src="/badge.svg" alt="badge"></Reduce>
+            <Reduce text="3. think twice before throwing out your clothes" 
+            src="/thinker.svg" alt="thinking"></Reduce>
+            <Reduce text="4. reduce the amount of times you do your laundry" 
+            src="/laundry.svg" alt="laundry"></Reduce>
           </div> 
-          <Info>Producing fast fashion items is to get as many products as they can into the market quickly. It means that caring about the environment, and the effects they have on it don’t play a part.
-</Info>  
-<ImgCont>
-<BgImg src="/pileofclothes.svg" alt="background image" />
-</ImgCont>
+
         </Background>
         <BottomNav />
     </div>
